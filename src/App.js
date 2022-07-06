215d836e4697a083437dee9fffc0862b71f1f125
Ro-bu <robin.raid1@gmail.com>
2022-07-06 14:24:56 +0300

    git error. restored from backup. UI created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from "react";
-import './App.css';
+import waveFront from "./img/wave-front.svg";
+import waveBack from "./img/wave-back.svg";
+import logo from "./img/logo.png";
+import clouds from "./img/clouds1.svg";
 
 function App() {
   return (
-    <div className="App">
+    <div className="main-container">
+      <div className="sun">
+
+      </div>
+      <img className="clouds" src={clouds} />
+      <img className="logo" src={logo} />
+      <div className="wave-front-container">
+        <img className="wave" src={waveFront} />
+      </div>
+      <div className="wave-back-container">
+        <img className="wave" src={waveBack} />
+      </div>
     </div>
   );
 }

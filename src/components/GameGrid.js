49a738b05ade2b75d81718f12b5de2d04d6eb6fa
Ro-bu <robin.raid1@gmail.com>
2022-07-08 00:23:18 +0300

    randomize data array on each render

--- src/components/GameGrid.js
+++ src/components/GameGrid.js
@@ -3,8 +3,20 @@ import {GameTile} from "../components/GameTile";
 import {data} from "../components/data";
 
 function GameGrid() {
+    // Fisher-Yates array shuffle
+    function shuffle(array) {
+        let m = array.length, t, i;
+        while (m) {
+        i = Math.floor(Math.random() * m--);
+        t = array[m];
+        array[m] = array[i];
+        array[i] = t;
+        }
+        return array;
+    }
+    let tileElements = shuffle(data);
 
-    let tileElements = data.map((animal) => {
+    tileElements = data.map((animal) => {
         return(
             <GameTile src={animal.src} id={animal.name} key={animal.name} />
         )

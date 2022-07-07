e1e1add24919c1fba4ac9dd00aae623c52a37872
Ro-bu <robin.raid1@gmail.com>
2022-07-07 20:16:42 +0300

    styled modal

--- src/components/HelpModal.js
+++ src/components/HelpModal.js
@@ -3,9 +3,9 @@ import React from "react";
 function HelpModal() {
 
     return (
-        <div className="mocal-container">
+        <div className="modal-container">
             <div className="modal">
-                <div className="close-modal"></div>
+                <div className="close-modal">X</div>
                 <h3 className="modal-title">How to play</h3>
                 <p className="modal-text">The object of the game is to click on tiles without repeating the same animal. After each click the board is shuffled and you have to remember the animals you have already clicked.</p>
             </div>

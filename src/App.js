1d06b8485eaaaa1660c061ec7e562c4b8dcd2acc
Ro-bu <robin.raid1@gmail.com>
2022-07-07 19:59:48 +0300

    add help button for modal activation

--- src/App.js
+++ src/App.js
@@ -5,20 +5,25 @@ import logo from "./img/logo.png";
 import clouds from "./img/clouds1.svg";
 import {ScoreBoard} from "./components/ScoreBoard";
 import {GameGrid} from "./components/GameGrid";
+import {HelpModal} from "./components/HelpModal";
 
 function App() {
   return (
     <div className="main-container">
+      <HelpModal />
       <div className="sun">
         <ScoreBoard />
       </div>
-      <img className="clouds" src={clouds} />
-      <img className="logo" src={logo} />
+      <img className="clouds" alt="cloud sprite" src={clouds} />
+      <div className="logo-container">
+        <img className="logo" alt="animal memory logo" src={logo} />
+        <div className="help-button" >?</div>
+      </div>
       <div className="wave-front-container">
-        <img className="wave" src={waveFront} />
+        <img className="wave" alt="wave-sprite" src={waveFront} />
       </div>
       <div className="wave-back-container">
-        <img className="wave" src={waveBack} />
+        <img className="wave" alt="wave-sprite" src={waveBack} />
       </div>
       <GameGrid />
     </div>
